e8e7feedcac3a60b70db4109df7b61f13882a44b
vitaoa <jupiterzw@sina.com>
2016-08-16 17:50:15 +0800

    index

---
+++ js/respond.js
@@ -0,0 +1,16 @@
++ function($) {
+    "use strict";
+    var applyMedia = function() {
+        var _w = $(window).width(),
+            _h = $(window).height();
+            
+//      _h <= 899 && _h >= 768 && ();
+//      _h <= 767 && ();
+//      _h <= 720 && ();
+//      _h <= 667 && ();
+    };
+    applyMedia();
+    $(window).resize(function(){
+        applyMedia();
+    });
+}(jQuery)
